bc955be42d08948c70249a447069dee5117a38cf
Kira <kawoods826@gmail.com>
2019-09-17 11:56:34 -0700

    added function for new user cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -27,17 +27,7 @@ axios.get('https://api.github.com/users/kirawoods')
            create a new component and add it to the DOM as a child of .cards
 */
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
 
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -119,10 +109,28 @@ axios.get('https://api.github.com/users/kirawoods')
         console.log("Error:", error);
     })
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
+  
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
 */
+
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach((element) => {
+  axios.get('https://api.github.com/users/' + element)
+    .then( response => {
+      let newFriendUserCard = userCardCreator(response);
+      cardContainer.appendChild(newFriendUserCard);
+    })
+    .catch( error => {
+        console.log("Error:", error);
+    })
+});
+
